31546c90965306d38a97e7e64622d6537465ebec
muvikt <muvikt@gmail.com>
2016-01-05 15:45:31 +0100

    printing changed

--- script.py
+++ script.py
@@ -3,12 +3,15 @@
 
 from lxml import etree
 from py2neo import Graph
+from nltk.corpus import wordnet as wn
 
 def parseXML(file):
   tree = etree.parse(file)
   dic={}
   
   print tree.xpath("SYNSET/SYNONYM/LITERAL/text()")
+  for synset in  tree.xpath("SYNSET/ID/text()"):
+    print synset
 
   
 #def createGraph():
