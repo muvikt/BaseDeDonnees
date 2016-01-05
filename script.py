4763ae496277b2c67bf786c8aff6c6fa623d642d
muvikt <muvikt@gmail.com>
2016-01-05 14:47:35 +0100

    First commit bis

--- script.py
+++ script.py
@@ -2,16 +2,16 @@
 # -*- coding: utf-8 -*-
 
 from lxml import etree
+from py2neo import Graph
 
 def parseXML(file):
   tree = etree.parse(file)
-  print tree.xpath("SYNSET/SYNONYM/LITERAL/text()")
-  #for temps in tree.xpath("/TimeML/TLINK"):
-    #nodeIn=temps.get("eventInstanceID")
-    #nodeOut=temps.get("relatedToTime")
-    #rel=temps.get("relType")
-    #if rel=='BEFORE' or rel=='AFTER':
-      #graph.add_edge(nodeIn, nodeOut, relaltion=rel)
-  #print graph.nodes()
+  dic={}
   
+  print tree.xpath("SYNSET/SYNONYM/LITERAL/text()")
+
+  
+#def createGraph():
+  
+ 
 parseXML("wolf-1.0b4.xml")
